bd5f2beedb411a2e0c262dee3e74795bad80deed
jurgenstevens <jurgen.stevens@yahoo.com>
2023-08-22 01:04:55 -0500

    Update contact settings to successfully receive emails.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_xj0fuo8', // paste your ServiceID here (you'll get one when your service is created).
+        'template_l7536op', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Jurgen', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'northemerican@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        '719U1PT_N2UQx3V-5' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
